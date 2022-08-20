208346b5e46b9126e930a740136fb4826d0554d5
NourH2000 <ninamimy43@gmail.com>
2022-08-20 04:59:15 +0100

    ppa one training and model

--- routes/PPa/OneTraining.js
+++ routes/PPa/OneTraining.js
@@ -77,6 +77,44 @@ router.get("/CountTS/", (req, res) => {
     });
 });
 
+// find the tp of drugs suspected
+router.get("/CountTP/", (req, res) => {
+  const query =
+    "select tier_payant  from ppa_result where id_entrainement = ?  ALLOW FILTERING ;";
+  const idEntrainement = req.query.idEntrainement;
+
+  client
+    .execute(query, [idEntrainement], { prepare: true })
+    .then((result) => {
+      console.log(result);
+      var ResultCountTs = result;
+      //The row is an Object with column names as property keys.
+      res.status(200).send(ResultCountTs?.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR :", err);
+    });
+});
+
+// find the pharmacy or agent cnas  of drugs suspected
+router.get("/CountPhar/", (req, res) => {
+  const query =
+    "select codeps  from ppa_result where id_entrainement = ?  ALLOW FILTERING ;";
+  const idEntrainement = req.query.idEntrainement;
+
+  client
+    .execute(query, [idEntrainement], { prepare: true })
+    .then((result) => {
+      console.log(result);
+      var ResultCountTs = result;
+      //The row is an Object with column names as property keys.
+      res.status(200).send(ResultCountTs?.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR :", err);
+    });
+});
+
 // get count of each region By medication ( center , medication )
 router.get("/CountCenterSuspected/", (req, res) => {
   const query =
@@ -122,14 +160,27 @@ router.get("/CountCenterSuspected/", (req, res) => {
 
 // get count of each region ( region => num_enr ) where grouoedByMedicaùent
 router.get("/CountOneCenterMedication/", (req, res) => {
-  const query =
-    "select count(*) , num_enr    from ppa_result where id_entrainement =? and region = ? group by num_enr      ALLOW FILTERING ;";
 
   const idEntrainement = req.query.idEntrainement;
   const region = req.query.region;
+  //console.log(req.query.region)
+  
+
+  if(req.query.region == 0){
+    var query =
+    "select count(*) , num_enr   from ppa_result where id_entrainement =?  group by num_enr  ALLOW FILTERING ;";
+    var param = [idEntrainement]
+    console.log("am here ")
+  }else{
+    var query =
+    "select count(*) , num_enr   from ppa_result where id_entrainement =? and region = ?  group by num_enr  ALLOW FILTERING ;"
+    var param = [idEntrainement, region]
+  }
+  
+  
 
   client
-    .execute(query, [idEntrainement, region], { prepare: true })
+    .execute(query, param, { prepare: true })
     .then((result) => {
       console.log(result);
       var ResultCountPerAssure = result;
